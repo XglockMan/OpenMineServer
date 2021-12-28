f14a01c69c2a685ae84a9c89aa34edc6d435fc6e
XglockMan <xglocman@gmail.com>
2021-12-28 16:49:17 +0100

    Add Plugin API

---
+++ OpenMinePluginAPI/OMPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenMinePluginAPI
+{
+    public abstract class OMPlugin
+    {
+        
+        public abstract void enable();
+
+        public abstract void disable();
+        
+    }
+}
